a6dbc12ffb640f61f5b162ee0a6e5a841df9df58
tevfik <tevfikhoglu@gmail.com>
2022-06-14 23:07:04 +0300

    jwt middleware

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -29,6 +29,8 @@ export const errorHandler = (
     return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' })
   }
   next(error)
 }
